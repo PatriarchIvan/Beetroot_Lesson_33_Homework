7a67e07bf7049a5f9bfa52a88928bfbca36ae525
Eugene <edukalis@gmail.com>
2022-10-11 11:15:42 +0300

    completed ivan tasks. now moving to LMS tasks

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
-// const arr = [];
-// arr.push(0, 1, 2, 3, 4);
-// console.log(`Array: ${arr}. Array's length: ${arr.length} elements`);
-// arr.push(5,6);
-// arr.shift();
-// console.log(`New Array: ${arr}`);
-
+const arr = [];
 const students = [
     {name: 'Hulio', surname: 'Avares', age: 22},
     {name: 'Pedro', surname: 'Ronaldo', age: 44},
@@ -12,30 +6,28 @@ const students = [
     {name: 'Margo', surname: 'Rapira', age: 33},
     {name: 'Huan', surname: 'Carleone', age: 19},
 ];
-
-// students.forEach(elem => {
-//     if (elem.age < 30) {
-//         console.log(`${elem.name} : ${elem.age}`);
-//     }
-// });
-
-
-let newStudents = [];
-
+const newStudents = [];
+// 1)
+arr.push(0, 1, 2, 3, 4);
+console.log(`Array: ${arr}. Array's length: ${arr.length} elements`);
+arr.push(5,6);
+arr.shift();
+console.log(`New Array: ${arr}. Array's length: ${arr.length} elements`);
+// 2)
+students.forEach(elem => {
+    if (elem.age < 30) {
+        console.log(`Yonger than 30: ${elem.name}, ${elem.age} years`);
+    }
+});
+// 3)
 for (let i = 0; i < students.length; i++) {
     newStudents[i] = students[i];
 }
-console.log(newStudents);
 
 for (let student of newStudents) {
     student.fullName = student.name + ' ' + student.surname;
     delete(student.name);
     delete(student.surname);
 }
-// newStudents.forEach(elem => {
-//     elem.fullName =  elem.name + ' ' + elem.surname;
-//     delete(elem.name);
-//     delete(elem.surname);
-// });
 
 console.log(newStudents);
